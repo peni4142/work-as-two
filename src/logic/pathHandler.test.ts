4c34fab9b324da1b3001e2a7838b1dcd621aa426
peni4142 <p.nissen.89@googlemail.com>
2020-01-25 10:51:32 +0100

    mocha eploror problem

---
+++ src/logic/pathHandler.test.ts
@@ -0,0 +1,15 @@
+import { equal } from "assert";
+
+
+
+suite("pathHandler", ()=>{
+    
+    test("just a test",()=> {
+        equal(5,5);
+    });
+    
+    test("just a test",()=> {
+        equal(5,5);
+    });
+
+});
