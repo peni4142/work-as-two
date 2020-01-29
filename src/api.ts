c8e8678048a777cb84abbf080a50db70cbdcfea6
peni4142 <p.nissen.89@googlemail.com>
2020-01-29 22:21:14 +0100

    identify opened by source control

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,7 @@ import { workspace, TextDocument, window, Uri } from "vscode";
 import { PathHandler } from "./logic/pathHandler";
 import { IMapPathResult } from "./logic/interfaces";
 import { HandSide } from "./logic/enum";
+import { CancelableQueue } from "./logic/cancelableQueue";
 
 
 
@@ -9,6 +10,9 @@ import { HandSide } from "./logic/enum";
 export class API {
 
     private static lastOpened: string = "";
+
+
+
     private static ph: PathHandler = new PathHandler({
         pathRight: {
             escapedRegex: /^([^.]*)(\.ts)/,
@@ -20,22 +24,33 @@ export class API {
         }
     });
 
-
     public static run() {
-        workspace.onDidOpenTextDocument((e: TextDocument) => {
-            let complemantary: IMapPathResult | null = this.ph.mapPath(e.fileName);
-            try {
-                if (complemantary && complemantary.path !== this.lastOpened) {
-                    workspace.openTextDocument(Uri.file(complemantary.path))
-                        .then((d: TextDocument) => {
-                            window.showTextDocument(d, complemantary?.handSide === HandSide.Left ? 1 : 2);
-                            window.showTextDocument(e, complemantary?.handSide === HandSide.Right ? 1 : 2);
-                        });
-                }
-            }
-            finally {
-                this.lastOpened = e.fileName;
+        workspace.onDidOpenTextDocument((document: TextDocument) => {
+            if (document.uri.scheme === "file") {
+                let windowCounter:number = window.visibleTextEditors.length;
+                setTimeout(()=>{
+                    let newWindowCounter: number = window.visibleTextEditors.length;
+                    if(newWindowCounter- windowCounter !== 2){
+                        this.open(document);
+                    }
+                }, 100);
             }
         });
+    }
+
+    private static open(document: TextDocument) {
+        let complemantary: IMapPathResult | null = this.ph.mapPath(document.fileName);
+        try {
+            if (complemantary && complemantary.path !== this.lastOpened) {
+                workspace.openTextDocument(Uri.file(complemantary.path))
+                    .then((d: TextDocument) => {
+                        window.showTextDocument(d, complemantary?.handSide === HandSide.Left ? 1 : 2);
+                        window.showTextDocument(document, complemantary?.handSide === HandSide.Right ? 1 : 2);
+                    });
+            }
+        }
+        finally {
+            this.lastOpened = document.fileName;
+        }
     }
 }
