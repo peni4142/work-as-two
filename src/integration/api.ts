cd8df75eadb0213d6cc4e4d93ff2751a87091ef1
peni4142 <p.nissen.89@googlemail.com>
2020-02-03 13:06:29 +0100

    hopefully everything is alright

--- src/integration/api.ts
+++ src/integration/api.ts
@@ -1,4 +1,4 @@
-import { workspace, TextDocument, window, Uri } from "vscode";
+import { workspace, TextDocument, window, Uri, TextEditor } from "vscode";
 import { PathHandler } from "../logic/pathHandler";
 import { IMapPathResult } from "../logic/interfaces";
 import { HandSide } from "../logic/enum";
@@ -9,70 +9,56 @@ import { IConfig } from "../interface";
 export class API {
 
     private static lastOpenedComplementary: string = "";
-    private static gitMode: boolean = false; // git mode is more precise, but it has to be in a git repository
-
-
-    private static windowCounter: number[] = [
-        window.visibleTextEditors.length,
-        window.visibleTextEditors.length
-    ];
+    private static prevState: TextEditor[] = window.visibleTextEditors;
 
     public static onDidShowTextDocument() {
-        setInterval(() => {
-            this.windowCounter.push(window.visibleTextEditors.length);
-            this.windowCounter.shift();
-            this.lastOpenedComplementary = "";
-        }, 300);
-
-        workspace.onDidOpenTextDocument((document: TextDocument) => {
-            if (document.uri.scheme === "gitfs") { // fires after every show up of a TextDocument
-                if (!this.gitMode) {
-                    this.gitMode = true;
-                } else { // the file mode will handle the first time
-                    let newWindowCounter: number = window.visibleTextEditors.length;
-                    if (newWindowCounter - this.windowCounter[0] === 1) { // prevent to act by source control
-                        workspace.openTextDocument(Uri.file(document.fileName.substr(0, document.fileName.length - 4))) // cut out ".git"
-                            .then((innerDocument: TextDocument) => {
-                                this.businessLogik(innerDocument);
-                            });
+        window.onDidChangeVisibleTextEditors((actualState:TextEditor[])=>{
+            try{                
+                if (actualState.length - this.prevState.length === 1) {
+                    for (let i: number = 0; i < actualState.length; i++) {
+                        if (!this.documentIsOpened(actualState[i].document, this.prevState)) {
+                            this.businessLogik(actualState[i].document);
+                            return;
+                        }
                     }
                 }
             }
-            if (!this.gitMode) {
-                if (document.uri.scheme === "file") { // fires every time when TextDocument is opened, but TextDocuments can stay open while you kill the VisibleTextEditor
-
-                    let windowCounter: number = window.visibleTextEditors.length;
-                    setTimeout(() => {
-                        if (window.visibleTextEditors.length - windowCounter === 1) {// prevent to act by source control
-                            this.businessLogik(document);
-                        }
-                    }, 100);
-                }
+            finally{
+                this.prevState = actualState;
             }
         });
     }
 
-    public static businessLogik(document: TextDocument) {
-            let config: IConfig = <any>workspace.getConfiguration("work-as-two", document.uri);
-            let ph: PathHandler = new PathHandler({
-                pathLeft: {
-                    escapedRegex: new RegExp(config.leftEscapedRegex),
-                    result: config.leftTransformToRight
-                },
-                pathRight: {
-                    escapedRegex: new RegExp(config.rightEscapedRegex),
-                    result: config.rightTransformToLeft
-                }
-            });
-            let complemantary: IMapPathResult | null = ph.mapPath(document.fileName);
-
-            if (complemantary && document.fileName !== this.lastOpenedComplementary) {
-                workspace.openTextDocument(Uri.file(complemantary.path))
-                    .then((complemantaryDocument: TextDocument) => {
-                        window.showTextDocument(complemantaryDocument, complemantary?.handSide === HandSide.Left ? 1 : 2);
-                        window.showTextDocument(document, complemantary?.handSide === HandSide.Right ? 1 : 2);
-                        this.lastOpenedComplementary = <string>complemantary?.path;
-                    });
+    private static documentIsOpened(document: TextDocument, textEditors: TextEditor[]): boolean {
+        for (let i: number = 0; 0 < textEditors.length; i++) {
+            if (document.fileName === textEditors[i].document.fileName) {
+                return true;
             }
+        }
+        return false;
+    }
+
+    public static businessLogik(document: TextDocument) {
+        let config: IConfig = <any>workspace.getConfiguration("work-as-two", document.uri);
+        let ph: PathHandler = new PathHandler({
+            pathLeft: {
+                escapedRegex: new RegExp(config.leftEscapedRegex),
+                result: config.leftTransformToRight
+            },
+            pathRight: {
+                escapedRegex: new RegExp(config.rightEscapedRegex),
+                result: config.rightTransformToLeft
+            }
+        });
+        let complemantary: IMapPathResult | null = ph.mapPath(document.fileName);
+
+        if (complemantary && document.fileName !== this.lastOpenedComplementary) {
+            workspace.openTextDocument(Uri.file(complemantary.path))
+                .then((complemantaryDocument: TextDocument) => {
+                    window.showTextDocument(complemantaryDocument, complemantary?.handSide === HandSide.Left ? 1 : 2);
+                    window.showTextDocument(document, complemantary?.handSide === HandSide.Right ? 1 : 2);
+                    this.lastOpenedComplementary = <string>complemantary?.path;
+                });
+        }
     }
 }
